4e43564c221f1ce91ab2ef3db52d7771de254eb7
zhangzhengyang <zhangzhengyang27@aliyun.com>
2023-02-27 20:11:04 +0800

    处理请求 url 参数问题

--- examples/server.js
+++ examples/server.js
@@ -17,11 +17,36 @@ app.use(webpackDevMiddleware(compiler, {
   }
 }))
 
+registerBaseRouter()
+
 router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
   })
 })
+
+function registerBaseRouter() {
+  router.get('/base/get', function(req, res) {
+    res.json(req.query)
+  })
+
+  // router.post('/base/post', function(req, res) {
+  //   res.json(req.body)
+  // })
+  //
+  // router.post('/base/buffer', function(req, res) {
+  //   let msg = []
+  //   req.on('data', (chunk) => {
+  //     if (chunk) {
+  //       msg.push(chunk)
+  //     }
+  //   })
+  //   req.on('end', () => {
+  //     let buf = Buffer.concat(msg)
+  //     res.json(buf.toJSON())
+  //   })
+  // })
+}
 
 app.use(router)
 
